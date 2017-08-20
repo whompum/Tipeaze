5648de3bd4eb2b567b45151b8d92abed0cbaf649
whompum <whompum101@gmail.com>
2017-08-20 10:17:53 -0400

    wrong branch... Might as well commit

---
+++ app/src/main/java/com/example/bryan/tipeaze/Presets/PresetLoader.java
@@ -0,0 +1,8 @@
+package com.example.bryan.tipeaze.Presets;
+
+/**
+ * Created by bryan on 8/19/2017.
+ */
+
+public class PresetLoader {
+}
---
+++ app/src/main/java/com/example/bryan/tipeaze/Presets/PresetQueryResponseInterface.java
@@ -0,0 +1,8 @@
+package com.example.bryan.tipeaze.Presets;
+
+/**
+ * Created by bryan on 8/20/2017.
+ */
+
+public interface PresetQueryResponseInterface {
+}
